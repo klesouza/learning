5d81b94d7e0bdb5ed8073b3ea49500c38e015f86
Kleber Silva <kle_zoio@hotmail.com>
2013-01-30 21:25:41 -0200

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klebersouza::Application.config.secret_token = '7bff89babfe4b015b695d9c5485db3ff3a8bf0efd6ffafccc1aa726bae99c3a8ea4ee07e815d7ff6af24465060922f25d6f0bf7f0e5a9806f0b7606b8edf14fc'
